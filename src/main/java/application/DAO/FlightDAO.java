7e1f127584663f5541ad79b006217630ab9c0c4d
monju1125 <monju1125@github.com>
2026-06-12 06:20:48 +0000

    Test run: 6/13 passed, 7 failed, 0 skipped - 2026-06-12T06:20:48.065Z

--- src/main/java/application/DAO/FlightDAO.java
+++ src/main/java/application/DAO/FlightDAO.java
@@ -157,7 +157,7 @@ public class FlightDAO {
             //preparedStatement.setInt(1, id);
             preparedStatement.setString(2, flight.getDeparture_city());
             preparedStatement.setString(3, flight.getArrival_city());
-            preparedStatement.setInt(3, id);
+            preparedStatement.setInt(3, flight.getFlight_id());
 
             preparedStatement.executeUpdate();
         }catch(SQLException e){
