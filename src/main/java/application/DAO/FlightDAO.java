0e53e5b3bf1105076584d1715d2f05113f19ca5f
monju1125 <monju1125@github.com>
2026-06-12 06:24:19 +0000

    Test run: 9/13 passed, 4 failed, 0 skipped - 2026-06-12T06:24:19.707Z

--- src/main/java/application/DAO/FlightDAO.java
+++ src/main/java/application/DAO/FlightDAO.java
@@ -110,7 +110,7 @@ public class FlightDAO {
         try {
             //Write SQL logic here. When inserting, you only need to define the departure_city and arrival_city
             //values (two columns total!)
-            String sql = "INSERT INTO flight(departure_city, arrival_city) VALUES(?, ?)" ;
+            String sql = "INSERT INTO flight(departure_city, arrival_city) VALUES(?, ?);" ;
             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             //write preparedStatement's setString and setInt methods here.
