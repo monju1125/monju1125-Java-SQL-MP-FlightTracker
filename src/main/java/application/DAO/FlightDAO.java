7f849e56454267c0bcbd812e5b58ae70f5996256
monju1125 <monju1125@github.com>
2026-06-12 06:19:07 +0000

    Test run: 6/13 passed, 7 failed, 0 skipped - 2026-06-12T06:19:07.185Z

--- src/main/java/application/DAO/FlightDAO.java
+++ src/main/java/application/DAO/FlightDAO.java
@@ -154,9 +154,10 @@ public class FlightDAO {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write PreparedStatement setString and setInt methods here.
-            preparedStatement.setInt(1, id);
+            //preparedStatement.setInt(1, id);
             preparedStatement.setString(2, flight.getDeparture_city());
             preparedStatement.setString(3, flight.getArrival_city());
+            preparedStatement.setInt(3, id);
 
             preparedStatement.executeUpdate();
         }catch(SQLException e){
